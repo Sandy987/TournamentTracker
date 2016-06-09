2f42884ee6a2f4355921f3df11f3ac676fc32eb0
Conrad Johansson <Conrad.Johansson@vista.co>
2016-06-10 11:24:33 +1200

    added required function to interface

--- src/TournamentTracker/Services/Interfaces/IEloService.cs
+++ src/TournamentTracker/Services/Interfaces/IEloService.cs
@@ -2,6 +2,6 @@ namespace TournamentTracker.Services.Interfaces
 {
     public interface IEloService
     {
-        
+        EloResult CalcElo(int playerOneElo, int playerTwoElo, MatchWinner winner);
     }
 }
